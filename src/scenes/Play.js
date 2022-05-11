7852b140ebe033d71625888996ed78a8abcb1a17
spandemic <anloeng@ucsc.edu>
2022-05-10 23:54:36 -0700

    attempt at room camera focus (only works for rooms 1, 5, 9) ??????????

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,17 +14,22 @@ class Play extends Phaser.Scene {
 
         // set world bounds for camera and physics
         this.cameras.main.setBounds(0, 0, screenWidth * 3, screenHeight * 3);
+        // this.cameras.main.centerOn(0, 0);
+        // this.cameras.main.setSize(screenWidth, screenHeight);
         this.physics.world.setBounds(0, 0, screenWidth * 3, screenHeight * 3);
 
-        this.add.image(0, 0, 'betaRoom').setOrigin(0);
-        this.add.image(0, screenHeight, 'betaRoom').setOrigin(0);
-        this.add.image(0, screenHeight * 2, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth, 0, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth, screenHeight, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth, screenHeight * 2, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth * 2, 0, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth * 2, screenHeight, 'betaRoom').setOrigin(0);
-        this.add.image(screenWidth * 2, screenHeight * 2, 'betaRoom').setOrigin(0);
+        // column 1
+        this.add.image(0, 0, 'room1').setOrigin(0);
+        this.add.image(0, screenHeight, 'room2').setOrigin(0);
+        this.add.image(0, screenHeight * 2, 'room3').setOrigin(0);
+        // column 2
+        this.add.image(screenWidth, 0, 'room4').setOrigin(0);
+        this.add.image(screenWidth, screenHeight, 'room5').setOrigin(0);
+        this.add.image(screenWidth, screenHeight * 2, 'room6').setOrigin(0);
+        // column 3
+        this.add.image(screenWidth * 2, 0, 'room7').setOrigin(0);
+        this.add.image(screenWidth * 2, screenHeight, 'room8').setOrigin(0);
+        this.add.image(screenWidth * 2, screenHeight * 2, 'room9').setOrigin(0);
 
         this.player = this.physics.add.sprite(centerX, centerY, 'cube');
         this.player.setOrigin(0.5);
@@ -32,10 +37,12 @@ class Play extends Phaser.Scene {
         this.player.setTint(0xF73D6E);
 
         this.player.setCollideWorldBounds(true);
-        this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+        // this.cameras.main.startFollow(this.player, true, 1, 1, screenWidth, screenHeight);
     }
 
     update() {
+        // console.log(this.player.x, this.player.y);
+        console.log(this.cameras.main.x, this.cameras.main.y);
         this.player.setVelocity(0);
 
         if (keyA.isDown) {
@@ -50,5 +57,53 @@ class Play extends Phaser.Scene {
         if (keyS.isDown) {
             this.player.setVelocityY(500);
         }
+
+        // camera panning logic
+        // row 1
+        if (this.player.x <= screenWidth) {
+            // col 1 (room1)
+            if (this.player.y <= screenHeight) {
+                this.cameras.main.centerOn(screenWidth / 2, screenHeight / 2);
+            }
+            // col 2 (room2)
+            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
+                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight / 2);
+            }
+            // col 3 (room3)
+            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
+                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight / 2);
+            }
+        }
+        // row 2
+        if (this.player.x > screenWidth && this.player.x <= screenWidth * 2) {
+            // col 1 (room4)
+            if (this.player.y <= screenHeight) {
+                this.cameras.main.centerOn(screenWidth / 2, screenHeight * 6 / 4);
+            }
+            // col 2 (room5)
+            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
+                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight * 6 / 4);
+            }
+            // col 3 (room6)
+            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
+                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight * 6 / 4);
+            }
+        }
+        // row 3
+        if (this.player.x > screenWidth * 2 && this.player.x <= screenWidth * 3) {
+            // col 1 (room7)
+            if (this.player.y <= screenHeight) {
+                this.cameras.main.centerOn(screenWidth / 2, screenHeight * 15 / 6);
+            }
+            // col 2 (room8)
+            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
+                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight * 15 / 6);
+            }
+            // col 3 (room9)
+            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
+                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight * 15 / 6);
+            }
+        }
+        
     }
 }
