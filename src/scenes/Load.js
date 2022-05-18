40c3f505aa57f23124dfa0b1d75947ff982927f6
spandemic <anloeng@ucsc.edu>
2022-05-18 06:38:54 -0700

    Added wall collision, Added room teleporting, Broke tilemap pls fix

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -8,15 +8,6 @@ class Load extends Phaser.Scene {
         this.load.path = './assets/';
 
         this.load.image('cube', 'standardCube.png');
-        this.load.image('room1', 'room1.png');
-        this.load.image('room2', 'room2.png');
-        this.load.image('room3', 'room3.png');
-        this.load.image('room4', 'room4.png');
-        this.load.image('room5', 'room5.png');
-        this.load.image('room6', 'room6.png');
-        this.load.image('room7', 'room7.png');
-        this.load.image('room8', 'room8.png');
-        this.load.image('room9', 'room9.png');
         this.load.image('Inventory', 'Inventory.png');
         this.load.image('Inventorybox', 'Inventorybox.png');
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,27 +1,49 @@
 class Play extends Phaser.Scene {
-playScene
+// playScene
     constructor() {
         super("sceneA");
         Phaser.Scene.call(this, 'sceneA');
     }
 
     create() {
+
+        this.walkSound = this.sound.add('walk', {
+            mute: true,
+            volume: 0.4,
+            rate: 1.5,
+            loop: true
+        });
+        this.openDoorSound = this.sound.add('openDoor', {
+            mute: false,
+            volume: 0.5,
+            rate: 1,
+            loop: false
+        });
+        this.pickUpItemSound = this.sound.add('pickItem', {
+            mute: false,
+            volume: 0.5,
+            rate: 1,
+            loop: false
+        });
+        this.pickUpItemSound = this.sound.add('dropItem', {
+            mute: false,
+            volume: 0.5,
+            rate: 1,
+            loop: false
+        });
+
         // tilemap
         const map = this.add.tilemap("map", 64, 64, 30, 20);
         const tileset = map.addTilesetImage("doorway_effect_64_tileset", "64_tiles");
-        
-        // trying to figure out how to load in the tiles for the entrances/exits (doorways)
-        // maybe just put the doorways on a tile layer instead of object layer in tiled
-        let doorways = map.createFromObjects("doorways", {
-            name: "doorway"
-        });
 
-        const floorLayer = map.createLayer("floor/floor", tileset, 0, 0);
-        const rugLayer = map.createLayer("floor/rug", tileset, 0, 0,);
-        const wallLayer = map.createLayer("collisions/walls", tileset, 0, 0);
-        const wallFrameLayer = map.createLayer("collisions/wallFrames", tileset, 0, 0);
-        const topDecorLayer = map.createLayer("collisions/decor/top", tileset, 0, 0);
-        const bottomDecorLayer = map.createLayer("collisions/decor/bottom", tileset, 0,0);
+        const floorLayer = map.createLayer("floor/floor", tileset, 0, 0).setDepth(1);
+        const rugLayer = map.createLayer("floor/rug", tileset, 0, 0).setDepth(2);
+        const spawnDoorLayer = map.createLayer("floor/spawn doorways", tileset, 0, 0).setDepth(2);
+        const returnDoorLayer = map.createLayer("floor/return doorways", tileset, 0, 0).setDepth(2);
+        const wallLayer = map.createLayer("collisions/walls", tileset, 0, 0).setDepth(3);
+        const wallFrameLayer = map.createLayer("collisions/wallFrames", tileset, 0, 0).setDepth(4);
+        const topDecorLayer = map.createLayer("collisions/decor/top", tileset, 0, 0).setDepth(5);
+        const bottomDecorLayer = map.createLayer("collisions/decor/bottom", tileset, 0,0).setDepth(6);
 
         // player interaction keys
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -32,85 +54,62 @@ playScene
 
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-        
-        this.isWalking = false;
-        this.itemNum = 0;
+        wallFrameLayer.setCollisionByProperty({ collides: true });
+        spawnDoorLayer.setCollisionByProperty({ type: "spawn" });
+        returnDoorLayer.setCollisionByProperty({ type: "exit" });
+
 
         // set world bounds for camera and physics
         // tileSize is set in main.js
-        this.cameras.main.setBounds(4*tileSize, 11*tileSize, map.widthInPixels, map.heightInPixels);
-        // this.cameras.main.centerOn(0, 0);
-        // this.cameras.main.setSize(screenWidth, screenHeight);
-        this.physics.world.setBounds(0, 0, screenWidth * 3, screenHeight * 3);
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
-        /*
-        // column 1
-        this.add.image(0, 0, 'room1').setOrigin(0);
-        this.add.image(0, screenHeight, 'room4').setOrigin(0);
-        this.add.image(0, screenHeight * 2, 'room7').setOrigin(0);
-        // column 2
-        this.add.image(screenWidth, 0, 'room2').setOrigin(0);
-        this.add.image(screenWidth, screenHeight, 'room5').setOrigin(0);
-        this.add.image(screenWidth, screenHeight * 2, 'room8').setOrigin(0);
-        // column 3
-        this.add.image(screenWidth * 2, 0, 'room3').setOrigin(0);
-        this.add.image(screenWidth * 2, screenHeight, 'room6').setOrigin(0);
-        this.add.image(screenWidth * 2, screenHeight * 2, 'room9').setOrigin(0);
-        */
+        this.hallwaySpawn = map.findObject("spawnpoints", obj => obj.name === "Hallway spawn");
+        this.livingSpawn = map.findObject("spawnpoints", obj => obj.name === "Living room spawn");
+        this.bathroomSpawn = map.findObject("spawnpoints", obj => obj.name === "Bathroom spawn");
+        this.kitchenSpawn = map.findObject("spawnpoints", obj => obj.name === "Kitchen spawn");
 
-        this.player = this.physics.add.sprite(7*tileSize, 16*tileSize, 'cube');
+        this.spawnList = [
+            this.hallwaySpawn,
+            this.livingSpawn,
+            this.bathroomSpawn,
+            this.kitchenSpawn
+        ];
+
+
+        this.player = this.physics.add.sprite(this.hallwaySpawn.x, this.hallwaySpawn.y, 'cube');
         this.player.setOrigin(0.5);
         this.player.setDepth(20);
         this.player.setTint(0xF73D6E);
 
-        this.itemGroup = this.add.group({ runChildUpdate: true });
-        this.hitItemLogic = this.physics.add.overlap(
-            this.player,
-            this.itemGroup,
-            this.collectItem,
-            null,
-            this
-        );
+        this.isWalking = false;
+        this.itemNum = 0;
 
-        this.time.addEvent({
-            delay: 100,
-            callback: this.createItem,
-            callbackScope: this,
-            loop: true        
-        });
+        // this.itemGroup = this.add.group({ runChildUpdate: true });
+        // this.hitItemLogic = this.physics.add.overlap(
+        //     this.player,
+        //     this.itemGroup,
+        //     this.collectItem,
+        //     null,
+        //     this
+        // );
+
+        // this.time.addEvent({
+        //     delay: 100,
+        //     callback: this.createItem,
+        //     callbackScope: this,
+        //     loop: true        
+        // });
 
         this.player.setCollideWorldBounds(true);
-        // this.cameras.main.startFollow(this.player, true, 1, 1, screenWidth, screenHeight);
+        this.cameras.main.startFollow(this.player, true, 0.05, 0.05, 64, 64);
 
-        this.walkSound = this.sound.add('walk', {
-            mute: false,
-            volume: 0.4,
-            rate: 1.5,
-            loop: true
-        });
-
-        this.openDoorSound = this.sound.add('openDoor', {
-            mute: false,
-            volume: 0.5,
-            rate: 1,
-            loop: false
-        });
-
-        this.pickUpItemSound = this.sound.add('pickItem', {
-            mute: false,
-            volume: 0.5,
-            rate: 1,
-            loop: false
-        });
-
-        this.pickUpItemSound = this.sound.add('dropItem', {
-            mute: false,
-            volume: 0.5,
-            rate: 1,
-            loop: false
-        });
+        this.physics.add.collider(this.player, wallFrameLayer);
+        this.physics.add.collider(this.player, spawnDoorLayer, this.sendFromSpawn, null, this);
+        this.physics.add.collider(this.player, returnDoorLayer, this.returnToSpawn, null, this);
+        
      
-
+        // bugged
         this.input.once(Phaser.Input.Events.POINTER_DOWN, function () {
 
             this.scene.switch('sceneB');
@@ -156,65 +155,12 @@ playScene
         if (this.player.state === 0) {
             this.walkSound.stop();
             this.isWalking = false;
-        }
-        
-
-        if(Phaser.Input.Keyboard.JustDown(keyTAB)){
-            this.scene.switch('sceneB');
-            
-        }
-        
-        
-        // camera panning logic
-        /*
-        // col 1
-        if (this.player.x <= screenWidth) {
-            // row 1 (room1)
-            if (this.player.y <= screenHeight) {
-                this.cameras.main.centerOn(screenWidth / 2, screenHeight / 2);
-            }
-            // row 2 (room4)
-            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
-                this.cameras.main.centerOn(screenWidth / 2, screenHeight * 6 / 4);
-            }
-            // row 3 (room7)
-            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
-                this.cameras.main.centerOn(screenWidth / 2, screenHeight * 15 / 6);
-            }
-        }
-        // col 2
-        if (this.player.x > screenWidth && this.player.x <= screenWidth * 2) {
-            // row 1 (room2)
-            if (this.player.y <= screenHeight) {
-                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight / 2);
-            }
-            // row 2 (room5)
-            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
-                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight * 6 / 4);
-            }
-            // row 3 (room8)
-            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
-                this.cameras.main.centerOn(screenWidth * 6 / 4, screenHeight * 15 / 6);
-            }
-        }
-        // col 3
-        if (this.player.x > screenWidth * 2 && this.player.x <= screenWidth * 3) {
-            // row 1 (room3)
-            if (this.player.y <= screenHeight) {
-                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight / 2);
-            }
-            // row 2 (room6)
-            if (this.player.y > screenHeight && this.player.y <= screenHeight * 2) {
-                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight * 6 / 4);
-            }
-            // row 3 (room9)
-            if (this.player.y > screenHeight * 2 && this.player.y <= screenHeight * 3){
-                this.cameras.main.centerOn(screenWidth * 15 / 6, screenHeight * 15 / 6);
-            }
-        }
-        */
+        } 
+    
         
     }
+
+
     wake(input, scene) {
         input.once(Phaser.Input.Events.POINTER_DOWN, function () {
             scene.switch('sceneB');
@@ -222,5 +168,16 @@ playScene
         }, this);
     }
 
+    sendFromSpawn() {
+        let randomSpawn = this.spawnList[Math.ceil(Math.random() * (this.spawnList.length - 1))];
+ 
+        this.player.setX(randomSpawn.x);
+        this.player.setY(randomSpawn.y);
+    }
+
+    returnToSpawn() {
+        this.player.setX(this.hallwaySpawn.x);
+        this.player.setY(this.hallwaySpawn.y);
+    }
 
 }
